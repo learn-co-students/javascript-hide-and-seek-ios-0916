c46c403d0a7a513df6e30461dff49f9ecb190cbc
Johann Kerr <johannkerr@gmail.com>
2017-03-30 00:29:43 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+function increaseRankBy(n) {
+  const lists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0; i < lists.length; i++) {
+    let children = lists[i].children
+    for (let j = 0; j < children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
+
+
+
+
+
+
+
+
+
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
